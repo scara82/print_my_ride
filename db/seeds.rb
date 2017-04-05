6aef121d848ba1aad5df180a8e3195de32a97408
Zach Chrystall <z.chrystall@gmail.com>
2017-04-05 13:01:39 +1000

    created user - creator relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Material.destroy_all
+Creator.destroy_all
+CreatorMaterial.destroy_all
+
+User.create( name: 'Will', last_name: 'Smith', email: 'will@smith.co', password: 'pudding')
+User.create( name: 'Wilbur', last_name: 'Wallace', email: 'wilbur@wallace.co', password: 'pudding')
+
+Creator.create( user_id: User.first.id, printer: true, printer_model: 'printomatic5000', location: 'Melbourne', designer: false, rating: 5, payment_details: 'cash please', description: "I just really love to see the creepy stuff you guys make me print")
+
+Material.create(name: 'plastic')
+Material.create(name: 'aluminium')
+Material.create(name: 'iron')
+Material.create(name: 'titatnium')
+
+CreatorMaterial.create( user_id: Creator.first.user_id, material_id: Material.last.id )
