9102e74df109f2e955434b80f1620056499e1170
Marta Scaramella <marta.scaramella@gmail.com>
2017-04-05 12:01:25 +1000

    users controller vuews

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/users/new', to: 'users#new'
+  post '/users', to: 'userss#create'
+  delete '/users', to: 'users#destroy'
+
+
 end
