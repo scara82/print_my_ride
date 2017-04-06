54a0cea81741ed46537a77b94e0313ce66711cd2
Marta Scaramella <marta.scaramella@gmail.com>
2017-04-06 12:05:30 +1000

    job controller

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get '/creators/user_type', to: 'creators#new'
   post '/creators', to: 'creators#create'
 
+  root '/pages#home'
+
   resources :cads
 
   namespace :api do
